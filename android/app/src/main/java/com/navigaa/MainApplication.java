b01ae43564400841e965907b247224f100043b4a
Nhan Huynh Huu <hhnhan1@Nhans-iMac.local>
2018-06-29 13:35:42 +0700

    done fix issue

--- android/app/src/main/java/com/navigaa/MainApplication.java
+++ android/app/src/main/java/com/navigaa/MainApplication.java
@@ -3,6 +3,7 @@ package com.navigaa;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativenavigation.NavigationReactPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.github.wumke.RNImmediatePhoneCall.RNImmediatePhoneCallPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new NavigationReactPackage(),
             new ImagePickerPackage(),
             new VectorIconsPackage(),
             new RNImmediatePhoneCallPackage(),
